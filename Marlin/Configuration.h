b5ba948113168ab54e728c6c2379092920f39f91
Arjen Korevaar <a.korevaar@mephix.com>
2020-05-29 16:25:24 +0200

    Updated configuration
    Signed-off-by: Arjen Korevaar <a.korevaar@mephix.com>

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -759,8 +759,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-// #define DEFAULT_MAX_ACCELERATION      { 3000, 3000, 100, 10000 }
-#define DEFAULT_MAX_ACCELERATION      { 4000, 4000, 5, 10000} // CUSTOM: max acceleration
+#define DEFAULT_MAX_ACCELERATION      { 3000, 3000, 100, 10000 }
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
